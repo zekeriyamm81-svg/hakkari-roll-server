5df6434f2374e09d1e25d8983f61dccc8b58c1d4
zekeriyamm81-svg <zekeriyamm81@gmail.com>
2026-09-25 14:17:55 +0300

    V5.14.2: refresh PWA shell for mobile menu

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="hakkari-roll-v5141-smartscan-20260925";
+const CACHE="hakkari-roll-v5142-menu-20260925";
 const CORE=["/","/static/manifest.json","/static/icons/icon.svg"];
 
 self.addEventListener("install",e=>{
